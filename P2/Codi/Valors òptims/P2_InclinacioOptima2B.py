11c07c431720969e182b09fc40a1bddea013f856
laurefisika <laurarodriguezjordan2.0@gmail.com>
2025-01-06 00:04:07 +0100

    Update P2_InclinacioOptima2B.py

--- P2/Codi/Valors òptims/P2_InclinacioOptima2B.py
+++ P2/Codi/Valors òptims/P2_InclinacioOptima2B.py
@@ -9,7 +9,7 @@ Created on Mon Dec 30 13:49:00 2024
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Constants
+# Definim les constants
 latitud = 41 + 39 / 60 + 55.115 / 3600  # Latitud de Bigues i Riells en graus
 latitud_rad = np.radians(latitud)       # Latitud en radians
 gamma = np.radians(0)                   # Panell orientat cap al sud
@@ -19,10 +19,10 @@ I_0 = 6.320069973e7                     # Irradiància solar constant
 R_S = 6.957e8                           # Radi del Sol
 eta = 0.2                               # Eficiència del panell
 A = 2                                   # Àrea d'un panell solar (m^2)
-N1 = 1                                # Nombre de panells 
+N1 = 1                                  # Nombre de panells 
 
 
-# Carregar les dades de coordenades sol-terra
+# Per carregar les dades de coordenades Sol-Terra
 data = np.loadtxt('coordenades_bigues_sol.txt', comments='#', delimiter=' ')
 rx, ry, rz = data[:, 0], data[:, 1], data[:, 2]
 modul = np.sqrt(rx**2 + ry**2 + rz**2)
@@ -40,7 +40,7 @@ def elevacio_solar(delta, omega):
     sin_elevacio = np.sin(latitud_rad) * np.sin(delta) + np.cos(latitud_rad) * np.cos(delta) * np.cos(omega)
     return np.arcsin(sin_elevacio)  # En radians
 
-# Funció per calcular el cos de l'angle d'incidència
+# Funció per calcular el cosinus de l'angle d'incidència
 def cos_theta_i(delta, omega, beta, gamma):
     term1 = np.sin(delta) * np.sin(latitud_rad) * np.cos(beta)
     term2 = -np.sin(delta) * np.cos(latitud_rad) * np.sin(beta) * np.cos(gamma)
@@ -58,12 +58,12 @@ def calcular_potencia(angle_incident, N):
             P[dia, hora] = A * I_0 * (R_S / modul[dia * 24 + hora])**2 * eta * cos_max * N
     return P
 
-# Vector per provar diferents inclinacions (en graus)
+# Vector per a provar diferents inclinacions (en graus)
 betas = np.arange(38, 39, 0.01)  
 potencies_totals = []
 
-h=  395   #altura muntanya
-L= 4.68e3    #distància muntanya
+h=  395   #Altura muntanya [m]
+L= 4.68e3    #Distància muntanya [m]
 sigma=np.arctan(h/L)
 
 for beta_deg in betas:
@@ -88,7 +88,7 @@ for beta_deg in betas:
     angles_incidents = np.array(angles_incidents)
     angles_incidents = np.nan_to_num(angles_incidents, nan=90)
 
-    # Calcul de potència per un panell
+    # Càlcul de la potència per un panell
     P_T = calcular_potencia(angles_incidents, N1)
 
     # Potència total combinada
